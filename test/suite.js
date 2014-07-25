9902893fa0e85ab79889c2abb2f2363883c624f6
Adrian Lynch <adi_ady_ade@hotmail.com>
2014-07-25 14:21:26 +0100

    Testing with should.js

--- test/suite.js
+++ test/suite.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
+var should = require('should');
 var r4 = require('../lib/random4');
 
-describe('number returned', function() {
-	it('should equal 4', function() {
-		assert(r4.random4() === 4);
+describe('Calling random4()', function() {
+	it('should return the number 4', function() {
+		var result = r4.random4();
+		//assert(result === 4);
+		result.should.be.exactly(4).and.be.a.Number;
 	});
 });
